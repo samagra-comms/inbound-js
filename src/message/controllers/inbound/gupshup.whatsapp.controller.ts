41f5200748882b66acf7e700e059023621276e63
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2023-12-14 18:41:30 +0530

    Added feature to save whatsapp report to supabase.

--- src/message/controllers/inbound/gupshup.whatsapp.controller.ts
+++ src/message/controllers/inbound/gupshup.whatsapp.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, Post, Body, Logger } from '@nestjs/common';
-import { GSWhatsAppMessage } from '@samagra-x/gupshup-whatsapp-adapter';
-import { ConfigService } from '@nestjs/config';
+import { GSWhatsAppMessage, convertMessageToXMsg } from '@samagra-x/gupshup-whatsapp-adapter';
 import { GupshupWhatsappInboundService } from '../../services/inbound/gupshup.whatsapp.service';
+import { SupabaseService } from '../../../message/services/supabase.service';
+import { XMessage } from '@samagra-x/xmessage';
 
 @Controller('/inbound/gupshup/whatsapp')
 export class GupshupWhatsappInboundController {
     constructor(
-        private configService: ConfigService,
-        private readonly inboundService: GupshupWhatsappInboundService
+        private readonly inboundService: GupshupWhatsappInboundService,
+        private readonly supabaseService: SupabaseService,
     ) {}
     private readonly logger = new Logger(GupshupWhatsappInboundController.name);
 
@@ -18,9 +19,16 @@ export class GupshupWhatsappInboundController {
 
     @Post()
     async handleIncomingMessageData(@Body() requestData: GSWhatsAppMessage): Promise<any> {
+        this.logger.log(requestData)
+        // TODO: Find a better way to distinguish between whatsapp message and report.
 		if ("mobile" in requestData){
-            this.logger.log(requestData)
+            this.logger.log("Received whatsapp message from user.");
 			await this.inboundService.handleIncomingGsWhatsappMessage(requestData);
 		}
+        else {
+            this.logger.log("Received delivery report for whatsapp.");
+            const reportXmsg: XMessage = await convertMessageToXMsg(requestData);
+            await this.supabaseService.writeMessage(reportXmsg);
+        }
     }
 }
